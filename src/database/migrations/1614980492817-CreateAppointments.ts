a728e9de53a6ac8a5b15b3464bd0d1b8fea74577
Vitor Carlos <vitorcarlos1312@gmail.com>
2021-03-05 19:50:40 -0300

    Create cors from connect aplication

---
+++ src/database/migrations/1614980492817-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1614980492817 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
